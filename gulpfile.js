3639a5d63846f28df280146a9a09a405f67f7987
Michael Harrington <mharrington@infragistics.com>
2018-08-01 15:54:58 -0400

    various cleaning up

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-
 'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require("gulp-rename");
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
 	return gulp.src('./sass/**/*.scss')
 		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(rename({suffix: "-min"}))
 		.pipe(sourcemaps.write('.'))
-    	.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
